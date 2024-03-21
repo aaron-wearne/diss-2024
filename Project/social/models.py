927c1e515acf6ac1e5f9b57e421596ceb202e042
Errn123 <aaron.wearne@gmail.com>
2024-03-21 16:57:15 +0000

    post class

--- Project/social/models.py
+++ Project/social/models.py
@@ -11,3 +11,17 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.user.username
+
+class Interests(models.Model):
+    pass
+
+class Post(models.Model):
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image_post = models.ImageField(upload_to='post_images', blank=True)
+
+    def __str__(self):
+        return f"Post by {self.author} on {self.created_at}"
+    
