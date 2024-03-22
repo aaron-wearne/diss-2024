5371fae150275bbd22a162f991fb19f4e47bdbe1
Errn123 <aaron.wearne@gmail.com>
2024-03-22 18:18:39 +0000

    More model additions

--- Project/social/models.py
+++ Project/social/models.py
@@ -12,8 +12,6 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-class Interests(models.Model):
-    pass
 
 class Post(models.Model):
     author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
@@ -24,4 +22,25 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Post by {self.author} on {self.created_at}"
+    
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
+    
     
