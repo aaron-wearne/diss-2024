c5466a26962054ad3b42da92e130f17327f5ca8a
Errn123 <aaron.wearne@gmail.com>
2024-04-09 15:39:42 +0100

    message

---
+++ Project/social/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image_post = models.ImageField(upload_to='post_images', blank=True)
+
+    def __str__(self):
+        return f"Post by {self.author} on {self.created_at}"
+    
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
+    
+class Connections(models.Model):
+    follower = models.ForeignKey(UserProfile, related_name='following', on_delete=models.CASCADE)
+    followed = models.ForeignKey(UserProfile, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('follower', 'followed')
+    
+    def __str__(self):
+        return f"{self.follower} follows {self.followed}"
+    
+
+class Share(models.Model):
+    original_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='shares')
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='shared_posts')
+    comment = models.TextField(blank=True)
+    shared_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} shared on {self.shared_at}'
---
+++ README.md
@@ -0,0 +1,2 @@
+# diss-2024
+# Social media with focus on recomender engine
