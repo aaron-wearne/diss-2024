4b5f042361828af11a2c02a7be11f7d9cdefb382
Errn123 <aaron.wearne@gmail.com>
2024-06-11 13:20:00 +0100

    mid dev 2

--- Project/landing/views.py
+++ Project/landing/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'register/index.html')
+
