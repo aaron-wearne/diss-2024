a6600027926d9813ea4a15fa95aa3bce56eb642f
Errn123 <aaron.wearne@gmail.com>
2024-06-19 16:39:45 +0100

    temp fix

--- Project/app/settings.py
+++ Project/app/settings.py
@@ -91,13 +91,6 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-'''DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'socialmedia',
         'USER': 'root',
@@ -105,7 +98,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
     }
-}'''
+}
 
 
 # Password validation
