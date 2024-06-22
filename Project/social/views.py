6aff5c2e9b68632a80845985f6a05ee5d05e0b41
Errn123 <aaron.wearne@gmail.com>
2024-06-22 21:42:22 +0100

    Comment funtionality and delete pots/comments. Admin funtionality for each

--- Project/social/views.py
+++ Project/social/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View 
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
-
+from django.views.generic.edit import UpdateView, DeleteView
 
 # Create your views here.
 class PostListView(View):
@@ -39,11 +40,47 @@ class PostDetailView(View):
         post = Post.objects.get(pk=pk)
         form = CommentForm()
 
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+
 
         context = {
             'post': post,
             'form': form,
+            'comments': comments,
 
         }
 
         return render(request, 'social/post_detail.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.post = post
+            new_comment.save()
+        
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+
+        context = {
+            'post': post,
+            'form': form, 
+            'comments': comments,
+        }
+        return render(request, 'social/post_detail.html', context)
+
+class PostEditView(UpdateView):
+    model = Post
+    fields = ['body']
+    template_name = 'social/post_edit.html'
+
+    def get_success_url(self) -> str:
+        pk = self.kwargs['pk']
+        return reverse_lazy('post-detail', kwargs ={'pk':pk})
+    
+class PostDeleteView(DeleteView):
+    model = Post 
+    template_name = 'social/post_delete.html'
+    success_url = reverse_lazy('post-list')
