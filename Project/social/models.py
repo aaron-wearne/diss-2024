aad70c293f425e450dfd455d4bcdcdabb9084302
Errn123 <aaron.wearne@gmail.com>
2024-06-23 16:12:46 +0100

    working comment delete

--- Project/social/models.py
+++ Project/social/models.py
@@ -13,3 +13,6 @@ class Comment(models.Model):
     created_on = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+class Likes(models.Model):
+    pass
